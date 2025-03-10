8f903a6280d9f82eef01885c909b8c0932da2525
Evance katanga <evancehkatanga@gmail.com>
2025-03-10 08:50:24 +0300

    Completed exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        ShowGreeting();
+
+        string userFullName = GetUserName();
+        int favoriteNumber = GetUserFavoriteNumber();
+
+        int result = ComputeSquare(favoriteNumber);
+
+        OutputResult(userFullName, result);
+    }
+
+    static void ShowGreeting()
+    {
+        Console.WriteLine("Hello and welcome to the application!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Could you please tell us your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int GetUserFavoriteNumber()
+    {
+        Console.Write("What’s your most favorite number? ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        return number;
+    }
+
+    static int ComputeSquare(int number)
+    {
+        return number * number;
+    }
+
+    static void OutputResult(string userName, int squaredResult)
+    {
+        Console.WriteLine($"Hey {userName}, the result of squaring your number is {squaredResult}.");
     }
 }
